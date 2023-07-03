8b2e346e52bbd4bdf7a00803def6b9626897bf6c
An Nguyen <nguyen.an.1196@gmail.com>
2023-07-03 15:10:18 -0700

    Add example from module5 MSFT training

--- Program.cs
+++ Program.cs
@@ -1,26 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Humanizer;
+int result = Fibonacci(5);
+Console.WriteLine(result);
 
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Quantities:");
-HumanizeQuantities();
-
-Console.WriteLine("\nDate/Time Manipulation:");
-HumanizeDates();
-
-static void HumanizeQuantities()
+static int Fibonacci(int n)
 {
-    Console.WriteLine("case".ToQuantity(0));
-    Console.WriteLine("case".ToQuantity(1));
-    Console.WriteLine("case".ToQuantity(5));
-}
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
 
-static void HumanizeDates()
-{
-    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
-    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
-    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
-    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+    for (int i = 2; i < n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+
+    return n == 0 ? n1 : n2;
 }
---
+++ ProgramMsftPractice.cs
@@ -0,0 +1,32 @@
+// Step 3 in learning series https://learn.microsoft.com/en-us/training/modules/dotnet-files/3-exercise-file-system
+// Did not clone the project from https://github.com/MicrosoftDocs/mslearn-dotnet-files
+
+using System.IO;
+using System.Collections.Generic;
+
+var salesFiles = FindFiles("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
